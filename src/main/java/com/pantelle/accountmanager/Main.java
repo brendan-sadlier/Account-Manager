86fd21c24b3333a1e17f395b94dcee2b7909dce2
Brendan Sadlier <brendan.sadlier@ucdconnect.ie>
2022-05-27 23:36:16 +0100

    Fixed Maven Compile Error

--- src/main/java/com/pantelle/accountmanager/Main.java
+++ src/main/java/com/pantelle/accountmanager/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("login-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.show();
